804dac93dbe9f9ac116be4fbdc2dbc6bb1fc93f8
GasbroNinja <gasbroider@live.it>
2023-04-05 19:05:34 +0200

    Routes pack and not found page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import MyHomePage from "./components/MyHomePage";
-//import Profile from "./components/Profile";
-//SPIEGAZIONE ERRORI IN CONSOLE --> MyNav
 import "bootstrap/dist/js/bootstrap"
 import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
 
 
 function App() {
   return (
     <>
-    <MyNav />
-    <MyHomePage />
-    {/*<Profile />*/}
-
-
-    
+      <BrowserRouter>
+        <MyNav />
+        <Routes>
+          <Route path="/" element={<MyHomePage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="*"
+            element={<NotFound spacings="pt-5" variant="danger" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
